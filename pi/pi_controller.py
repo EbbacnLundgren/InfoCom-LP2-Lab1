8a78f94b3699169ebea3720b010480bd40bd913d
Ebba Lundgren <46447855+EbbacnLundgren@users.noreply.github.com>
2021-11-18 08:17:32 +0100

    update controller for pi stick
    tog bort elif c och la till direction med alla riktingar

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -2,30 +2,42 @@ import requests
 import time
 import random
 import click
+from sense_hat import SenseHat
+sense = SenseHat()
 
 
 def get_direction():
     d_long = 0
     d_la = 0
     send_vel = False
-    c = click.getchar()
-    if c == '\x1b[D' or c =='a':
-        click.echo('Left')
+    #c = click.getchar()
+    #if c == '\x1b[D' or c =='a':
+    while true:
+        for event in sense.stick.get_event():
+            if event.direction == "left":
+                print(event.direction, event.action)
+        #click.echo('Left')
         send_vel = True
         d_long = -1
         d_la = 0
-    elif c == '\x1b[C' or c == 'd':
-        click.echo('Right')
+    #elif c == '\x1b[C' or c == 'd':
+        #click.echo('Right')
+    elif event.direction == "right"
+        print(event.direction, event.action)
         send_vel = True
         d_long = 1
         d_la = 0
-    elif c == '\x1b[A' or c =='w':
-        click.echo('Up')
+    #elif c == '\x1b[A' or c =='w':
+        #click.echo('Up')
+    elif event.direction == "up"
+        print(event.direction, event.action)
         send_vel = True
         d_long = 0
         d_la = 1
-    elif c == '\x1b[B' or c == 's':
-        click.echo('Down')
+    #elif c == '\x1b[B' or c == 's':
+        #click.echo('Down')
+    elif event.direction == "down"
+        print(event.direction, event.action)
         send_vel = True
         d_long = 0
         d_la = -1
